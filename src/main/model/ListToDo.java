99b14213ccfa4572041962e2c9c80bbacc150757
Jacky Zhong <Jacky@MacBook-Air-2.local>
2024-10-08 10:24:14 -0700

    Added specification for ListToDo and Task to model

---
+++ src/main/model/ListToDo.java
@@ -0,0 +1,28 @@
+package main.model;
+import java.util.ArrayList;
+
+public class ListToDo {
+    private ArrayList<Task> list;
+
+    public ListToDo() {
+        this.list = new ArrayList<Task>();
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: 
+
+    // REQUIRES: task.done != true
+    // MODIFIES: this
+    // EFFECTS: add a task to the list to do
+    public void addTask() {
+        // TODO
+    }
+
+    // REQUIRES: task.done != true
+    // MODIFIES: this
+    // EFFECTS: remove a task to the list to do
+    public void removeTask() {
+        // TODO
+    }
+}
---
+++ src/main/model/Task.java
@@ -0,0 +1,48 @@
+package main.model;
+
+
+public class Task {
+    private String date;
+    private String name;
+    private int length;
+    private String time;
+    private boolean done;
+
+    public Task(String name, int length, String date, String time) {
+        this.name = name;
+        this.length = length;
+        this.date = date;
+        this.time = time;
+        this.done = false;
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: chnage the state of Done, either to true or false
+    public void setDone(){
+        // TODO
+    }
+
+    public String getName(){
+        // TODO
+        return "";
+    }
+
+    public int getLength(){
+        // TODO
+        return 0;
+    }
+
+    public String getDate(){
+        // TODO
+        return "";
+    }
+
+    public String getTime(){
+        // TODO
+        return "";
+    }
+
+
+
+
+}
