c9a6e7fb34c3d3ff91c4a7261152f912985552ca
Jacky Zhong <Jacky@dhcp-206-12-180-167.ubcsecure.wireless.ubc.ca>
2024-10-09 12:32:15 -0700

    implemented everything in the CalendarView apart from displayCalendar

--- src/main/ui/CalendarView.java
+++ src/main/ui/CalendarView.java
@@ -1,6 +1,7 @@
 package ui;
 import java.util.Scanner;
 import model.ListToDo;
+import model.Task;
 
 public class CalendarView {
     private ListToDo lst;
@@ -8,6 +9,7 @@ public class CalendarView {
 
     // EFFECTS: runs the user interface 
     public CalendarView() {
+        input = new Scanner(System.in);
         lst = new ListToDo();
         runCalendar();
     }
@@ -17,9 +19,9 @@ public class CalendarView {
     private void runCalendar() {
         boolean continueRunning = true;
         String command = null;
-
         while (continueRunning) {
-            updateCalendar(lst);
+            displayCalendar(lst);
+            System.out.println("a : add Task | m : mark task done | r : remove task");
             command = input.next().toLowerCase();
             if (command.equals("q")) {
                 continueRunning = false;
@@ -30,9 +32,16 @@ public class CalendarView {
         System.out.println("Thank you");
     } 
 
+    // EFFECTS: prints out list to do
+    public void displayList() {
+        for (int i = 0; i < lst.getList().size();i++ ) {
+            System.out.println(i + "|" + lst.getList().get(i).getName());
+        }
+    }
 
-    // EFFECTS: update and prints out calendar to match events in the list 
-    public void updateCalendar(ListToDo lst) {
+    // EFFECTS: prints out list to do in a calendar view 
+    public void displayCalendar(ListToDo lst) {
+        // TODO: currently hard coded, update to refelct lst content
         System.out.println("Monday | Tuesday | Wednesday | Thuresday | Friday | Saturday | Sunday");
         System.out.println("---------------------------------------------------------------------");
         System.out.println("       |         |           |           |        |          |       ");
@@ -52,13 +61,12 @@ public class CalendarView {
             case "a":
                 addingTask();
                 break;
-            case "c":
-                changeTask();
-                break;
-            case "d":
+            case "m":
                 markTaskDone();
                 break;
-
+            case "r":
+                removeTask();
+                break;
             default:
                 System.out.println("Invalid Input");
                 break;
@@ -68,20 +76,50 @@ public class CalendarView {
     // MODIFIES: this
     // EFFECTS: add a task to list
     public void addingTask() {
-        // TODO
+        System.out.println("Input task name");
+        String name = input.next().toLowerCase();
+        System.out.println("Input task length");
+        int length = input.nextInt();
+        System.out.println("Input date in the format 25/10/2024");
+        String date = input.next().toLowerCase();
+        System.out.println("Input time in the format 16:00, only whole hours");
+        String time = input.next().toLowerCase();
+        Task taskToAdd = new Task(name,length,date,time);
+        lst.addTask(taskToAdd);
     }
 
     // REQUIRES: lst.size() > 0
     // MODIFIES: this
-    // EFFECTS: change attributes of an existing task
-    public void changeTask() {
-        // TODO
+    // EFFECTS: remove a task from the list to do
+    public void removeTask() {
+        if (lst.getList().size() == 0){
+            System.out.println("Empty");
+        } else {
+            displayList();
+            System.out.println("Select by index");
+            int selected = input.nextInt();
+            Task taskSelected = lst.getList().get(selected);
+            lst.removeTask(taskSelected);
+        }
+
+
     }
 
     // REQUIRES: lst.size() > 0
     // MODIFIES: this
     // EFFECTS: mark a task in the list as done
     public void markTaskDone() {
-        // TODO
+        if (lst.getList().size() == 0){
+            System.out.println("Empty");
+        } else {
+            displayList();
+            System.out.println("Select by index");
+            int selected = input.nextInt();
+            Task taskSelected = lst.getList().get(selected);
+            taskSelected.setDone(true);
+        }
+
     }
+
+
 }
