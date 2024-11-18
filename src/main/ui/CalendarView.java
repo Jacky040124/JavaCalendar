59d6179876c8b9b573917e528b7913ed13c8566a
Jacky Zhong <Jacky@dhcp-206-12-182-210.ubcsecure.wireless.ubc.ca>
2024-11-18 14:57:41 -0800

    refactored comment line system to a seperate file

--- src/main/ui/CalendarView.java
+++ src/main/ui/CalendarView.java
@@ -21,97 +21,8 @@ public class CalendarView {
         jsonWriter = new JsonWriter(JSON_STORE);
         input = new Scanner(System.in);
         lst = new ListToDo();
-        runCalendar();
+        // runCalendar();
         
-    }
-
-    // MODIFIES: this
-    // EFFECTS: Continuously accepts user commands until the user chooses to quit.
-    private void runCalendar() {
-        boolean continueRunning = true;
-        String command = null;
-        while (continueRunning) {
-            displayCalendar();
-            System.out.println("a : add Task | m : mark task done | r : remove task | s : save task | l : load task");
-            command = input.next().toLowerCase();
-            if (command.equals("q")) {
-                continueRunning = false;
-            } else {
-                run(command);
-            }
-        }
-        System.out.println("Thank you");
-    } 
-
-    // EFFECTS: Displays the list of tasks.
-    public void displayList() {
-        for (int i = 0; i < lst.getList().size();i++) { 
-            System.out.println(i + "|" + lst.getList().get(i).getName());
-        }
-    }
-
-    // EFFECTS: Prints the calendar view with time slots and tasks for each day of the week.
-    public void displayCalendar() {
-        System.out.println("Time |Monday    |Tuesday   |Wednesday |Thuresday |Friday    |Saturday  |Sunday    ");
-        System.out.println("----------------------------------------------------------------------------------");
-
-        for (int i = 0; i <= 24; i++) { 
-            String strToPrint = String.format("%02d:00", i);
-            for (int j = 0; j < 7; j++) {
-                String key = Integer.toString(j) + ":" + Integer.toString(i);
-                if (lst.getAvailability().containsKey(key)) {
-                    String taskName = lst.getAvailability().get(key);
-
-                    boolean isDone = false;
-                    for (Task task : lst.getList()) {
-                        if (task.getName().equals(taskName) && task.getDone()) {
-                            isDone = true;
-                            break;
-                        }
-                    }
-
-                    if (isDone) {
-                        // Create strikethrough effect for completed tasks
-                        String crossedOut = taskName.chars()
-                                .mapToObj(ch -> (char) ch + "\u0336")
-                                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                                .toString();
-                        strToPrint += String.format("|%-10s", crossedOut);
-                    } else {
-                        strToPrint += String.format("|%-10s", taskName);
-                    }
-                    
-                } else {
-                    strToPrint += "|          ";
-                }
-            }
-            System.out.println(strToPrint);
-        }
-    }
-
-    // MODIFIES: this
-    // EFFECTS: Executes the user's command, 'a' to add a task, 'm' to mark a task as done, 'r' to remove a task
-    public void run(String command) {
-        switch (command) {
-            case "a":
-                addingTask();
-                break;
-            case "m":
-                markTaskDone();
-                break;
-            case "r":
-                removeTask();
-                break;
-            case "s":
-                saveListToDo();
-                break;
-            case "l":
-                loadListToDo();
-                break;
-            default:
-                System.out.println("Invalid Input");
-                break;
-        }
     }
 
     // MODIFIES: this
@@ -134,8 +45,6 @@ public class CalendarView {
         } else {
             System.out.println("busy");
         }
-    
-
     }
 
     // REQUIRES: lst is not empty.
@@ -145,8 +54,6 @@ public class CalendarView {
         if (lst.getList().size() == 0) {
             System.out.println("Empty");
         } else {
-            displayList();
-            System.out.println("Select by index");
             int selected = input.nextInt();
             Task taskSelected = lst.getList().get(selected);
             lst.removeTask(taskSelected);
@@ -162,8 +69,6 @@ public class CalendarView {
         if (lst.getList().size() == 0) { 
             System.out.println("Empty");
         } else {
-            displayList();
-            System.out.println("Select by index");
             int selected = input.nextInt();
             Task taskSelected = lst.getList().get(selected);
             taskSelected.setDone(true);
---
+++ src/main/ui/OldCalendarView.java
@@ -0,0 +1,95 @@
+// package ui;
+
+// import model.Task;
+
+// public class OldCalendarView {
+//     // MODIFIES: this
+//     // EFFECTS: Continuously accepts user commands until the user chooses to quit.
+//     private void runCalendar() {
+//         boolean continueRunning = true;
+//         String command = null;
+//         while (continueRunning) {
+//             displayCalendar();
+//             System.out.println("a : add Task | m : mark task done | r : remove task | s : save task | l : load task");
+//             command = input.next().toLowerCase();
+//             if (command.equals("q")) {
+//                 continueRunning = false;
+//             } else {
+//                 run(command);
+//             }
+//         }
+//         System.out.println("Thank you");
+//     } 
+
+//     // EFFECTS: Displays the list of tasks.
+//     public void displayList() {
+//         for (int i = 0; i < lst.getList().size();i++) { 
+//             System.out.println(i + "|" + lst.getList().get(i).getName());
+//         }
+//     }
+
+
+//     // EFFECTS: Prints the calendar view with time slots and tasks for each day of the week.
+//     public void displayCalendar() {
+//         System.out.println("Time |Monday    |Tuesday   |Wednesday |Thuresday |Friday    |Saturday  |Sunday    ");
+//         System.out.println("----------------------------------------------------------------------------------");
+
+//         for (int i = 0; i <= 24; i++) { 
+//             String strToPrint = String.format("%02d:00", i);
+//             for (int j = 0; j < 7; j++) {
+//                 String key = Integer.toString(j) + ":" + Integer.toString(i);
+//                 if (lst.getAvailability().containsKey(key)) {
+//                     String taskName = lst.getAvailability().get(key);
+
+//                     boolean isDone = false;
+//                     for (Task task : lst.getList()) {
+//                         if (task.getName().equals(taskName) && task.getDone()) {
+//                             isDone = true;
+//                             break;
+//                         }
+//                     }
+
+//                     if (isDone) {
+//                         // Create strikethrough effect for completed tasks
+//                         String crossedOut = taskName.chars()
+//                                 .mapToObj(ch -> (char) ch + "\u0336")
+//                                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+//                                 .toString();
+//                         strToPrint += String.format("|%-10s", crossedOut);
+//                     } else {
+//                         strToPrint += String.format("|%-10s", taskName);
+//                     }
+                    
+//                 } else {
+//                     strToPrint += "|          ";
+//                 }
+//             }
+//             System.out.println(strToPrint);
+//         }
+//     }
+
+//     // MODIFIES: this
+//     // EFFECTS: Executes the user's command, 'a' to add a task, 'm' to mark a task as done, 'r' to remove a task
+//     public void run(String command) {
+//         switch (command) {
+//             case "a":
+//                 addingTask();
+//                 break;
+//             case "m":
+//                 markTaskDone();
+//                 break;
+//             case "r":
+//                 removeTask();
+//                 break;
+//             case "s":
+//                 saveListToDo();
+//                 break;
+//             case "l":
+//                 loadListToDo();
+//                 break;
+//             default:
+//                 System.out.println("Invalid Input");
+//                 break;
+//         }
+//     }
+// }
